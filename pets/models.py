e87235a05f35502f6007f1b4af5308baec0d2da7
Enrique <enrique.barbosasilva@gmail.com>
2022-11-30 22:26:35 -0300

    refactor: code-changed

--- pets/models.py
+++ pets/models.py
@@ -2,13 +2,21 @@ from django.db import models
 
 # Create your models here.
 class SexOptions(models.TextChoices):
-    FEMALE = 'Female'
-    MALE = 'Male'
-    DEFAULT = 'Not Informed'
+    FEMALE = "Female"
+    MALE = "Male"
+    DEFAULT = "Not Informed"
 
 
 class Pet(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
     weight = models.FloatField()
-    sex = models.CharField(max_length=20, choices=SexOptions.choices, default=SexOptions.DEFAULT)
+    sex = models.CharField(
+        max_length=20, choices=SexOptions.choices, default=SexOptions.DEFAULT
+    )
+
+    group = models.ForeignKey(
+        "groups.Group", on_delete=models.CASCADE, related_name="groups"
+    )
+
+    traits = models.ManyToManyField("traits.Trait", related_name="traits_id", null=True)
