eb88e36f1a1b1a20726868573704d4ea24d5ba4a
Enrique <enrique.barbosasilva@gmail.com>
2022-12-01 15:11:01 -0300

    refactor: code-changed

---
+++ pets/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("pets/", views.PetView.as_view()),
+]
--- pets/views.py
+++ pets/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView, Response, Request, status
 
 # Create your views here.
+class PetView(APIView):
+    def get(self, request: Request) -> Response:
+        ...
+
+    
+    def post(self, request: Request) -> Response:
+        ...
