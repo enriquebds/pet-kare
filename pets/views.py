51ea1955436e1344faf9f69d20db246dc9e46ae2
Enrique <enrique.barbosasilva@gmail.com>
2022-12-02 15:20:04 -0300

    refactor: code-changed

--- pets/views.py
+++ pets/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView, Response, Request, status
 from .serializers import PetSerializer
 from pets.models import Pet
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 class PetView(APIView):
@@ -17,3 +18,27 @@ class PetView(APIView):
         serializer.save()
 
         return Response(serializer.data, status.HTTP_201_CREATED)
+
+class PetDetailView(APIView):
+    def get(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, id=pet_id)
+
+        serializer = PetSerializer(pet)
+
+        return Response(serializer.data)
+
+    def patch(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, id=pet_id)
+
+        serializer = PetSerializer(pet, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+
+        serializer.save()
+        return Response(serializer.data)
+
+    def delete(self, request: Request, pet_id: int) -> Response:
+        pet = get_object_or_404(Pet, id=pet_id)
+
+        pet.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
