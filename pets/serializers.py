71b49cfa17ea7baca4713f3aada0e85cd77cf22e
Enrique <enrique.barbosasilva@gmail.com>
2022-12-02 15:19:43 -0300

    refactor: code-changed

--- pets/serializers.py
+++ pets/serializers.py
@@ -16,13 +16,36 @@ class PetSerializer(serializers.Serializer):
         default=SexOptions.DEFAULT,
     )
     group = GroupSerializer()
-    traits = TraitSerializer(many=True, read_only=True)
+    traits = TraitSerializer(many=True)
 
     def create(self, validated_data: dict) -> Pet:
 
+        trait_dict = validated_data.pop("traits")
         group_dict = validated_data.pop("group")
+        group, _ = Group.objects.get_or_create(**group_dict)
 
-        pet_obj = Pet.objects.create(**validated_data)
-        group = Group.objects.create(**group_dict)
+        pet_obj = Pet.objects.create(**validated_data, group=group)
+
+        for trait in trait_dict:
+            trait, _ = Trait.objects.get_or_create(**trait)
+            pet_obj.traits.add(trait)
 
         return pet_obj
+
+    def update(self, instance: Pet, validated_data: dict):
+        group_dict: dict = validated_data.pop("group", None)
+
+
+        if group_dict:
+            group_obj, created = Group.objects.get_or_create(pet=instance)
+
+            for key, value in group_dict.items():
+                setattr(group_obj, key, value)
+            group_obj.save()
+
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
